fe00c3c0509434dfcecc3ac8cf22e9aab99dd95a
romeo2242 <57095620+romeo2242@users.noreply.github.com>
2019-11-12 17:29:26 +0100

    Add files via upload

---
+++ hotel/hotel.php
@@ -0,0 +1,30 @@
+<?php
+    $cognome = $_REQUEST["cognome"];
+    //$cognome = $_GET["cognome"];
+    //$cognome = $_POST["cognome"];
+
+    $nome = $_REQUEST["nome"];
+    $email = $_REQUEST["email"];
+    $numero = $_REQUEST["numero"];
+
+    $marca = $_REQUEST["hotel"];
+    $tipo = $_REQUEST["tipo"];
+
+    $optional = $_REQUEST["optional"];
+
+    $debug=1;
+    if($debug){
+        echo "nome: " . $nome . "<br>";
+        echo "cognome: " . $cognome . "<br>";
+        echo "email: " . $email . "<br>";
+        echo "numero: " . $numero . "<br>";
+        echo "hotel: " . $hotel . "<br>";
+        echo "tipo: " . $tipo . "<br>";
+
+        echo "optional: ";
+        foreach($opt as $optional){
+            echo $opt . ", ";
+        }
+
+
+    }
